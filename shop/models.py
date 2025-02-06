64379fe5ffe3f8c4ae75da1260ebd8bc306eb1a7
ArtmiIS <141933436+aminattaei@users.noreply.github.com>
2025-02-06 12:56:20 +0330

    Added Raw_materials and Nutritional_value

--- shop/models.py
+++ shop/models.py
@@ -10,14 +10,17 @@ class Food(models.Model):
         ("drinks", "نوشیدنی"),
         ("dessert", "دسر"),
     ]
-    
+
     title = models.CharField(_("نام غذا"), max_length=50)
     price = models.IntegerField(_("قیمت غذا"))
+    content = models.TextField(_("توضیحات غذا"), null=True, blank=True)
     created_time = models.DateTimeField(
         _("زمان ایجاد غذا"), auto_now=False, auto_now_add=True
     )
     image = models.ImageField(_("تصویر غذا"), upload_to="foods/")
 
+    Raw_materials = models.TextField(_("مواد اولیه"), null=True, blank=True)
+    Nutritional_value = models.TextField(_("ارزش غذایی"), null=True, blank=True)
     food_type = models.CharField(
         _("دسته غذا"), max_length=50, choices=TYPE_FOOD, null=True, blank=True
     )
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Food
 
+
 class HomeResturantListView(ListView):
     model = Food
-    context_object_name = 'foods'
-    template_name='Foods/shop_index.html'
+    context_object_name = "foods"
+    template_name = "Foods/Shop_index.html"
+
+
+class ProductDetailView(DetailView):
+    model = Food
+    context_object_name='food'
+    template_name = "foods/Shop_detail.html"
